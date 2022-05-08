c9c49f59f34e9b4494d0c47068b5604656ddc081
Satveer <satkaurdhillon@gmail.com>
2022-05-08 18:24:31 -0400

    sold out badge and mutiple cards

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import './index.css';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import Card from './components/Card'
-import card1 from './images/katie-zaferes.png'
+import data from './data'
+// import card1 from './images/katie-zaferes.png'
 // import star from './images/star.png'
 
 function App() {
+  const cards = data.map(item => {
+      return (<Card
+      key={item.id}
+      img ={item.coverImg}
+      rating={item.stats.rating}
+      reviews={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+      openSpots={item.openSpots}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
-      <Card
-        img ={card1}
-        star ="star.png"
-        rating="5.0"
-        reviews={6}
-        country="USA"
-        title="Life lessons with Kaite Zaferes"
-        price={136}
-      />
+      <section className='cards__list'>
+      {cards}
+      </section>
+      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,17 @@ import star from '../images/star.png'
 
 export default function Card(props){
     return(
-        <section className="card1">
-            <img src={props.img} className="card1__img"/>
-            <div>
-                <img src={star} className="star"/>
+        <section className="card">
+            {props.openSpots === 0 && <div className="card__badge">SOLD OUT</div>}
+            <img src={`./images/${props.img}`} className="card__img"/>
+            <div className="card__stats">
+                <img src={star} className="card__star"/>
                 <span>{props.rating} </span>
                 <span className="grey">({props.reviews}) •</span>
-                <span className="grey">{props.country}</span>
+                <span className="grey">{props.location}</span>
             </div>
-            <h2>{props.title}</h2>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <h2 className="card__title">{props.title}</h2>
+            <p className="card__price"><span className="bold">From ${props.price}</span> / person</p>
             
         </section>
     )
